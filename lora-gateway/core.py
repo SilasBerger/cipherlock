67a43d74d5e331fc26bbee49ae0582654e5d183a
Silas Berger <silas.berger@outlook.com>
2024-06-01 14:28:51 +0200

    Sketch some ideas for lora gateway.

---
+++ lora-gateway/core.py
@@ -0,0 +1,2 @@
+# Any core functionality that doesn't change depending on whether we are in the real ESP32 / LoRa environment or
+# running in test_main mode.
---
+++ lora-gateway/test_main.py
@@ -0,0 +1,7 @@
+import core
+
+# connect to game server (socket.io, role = lora)
+# receive expected box IDs from game server
+# send box online status updates to game server, REPL-style
+# receive unlock requests from game server
+# send unlock requests to WiFi-connected boxes
