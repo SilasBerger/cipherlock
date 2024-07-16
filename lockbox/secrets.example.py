d5e46197951b345679252e65da00f0529f7e2688
Silas Berger <silas.berger@outlook.com>
2024-07-16 11:51:25 +0200

    Add secrets example.

---
+++ lockbox/secrets.example.py
@@ -0,0 +1,5 @@
+WIFI_SSID = "my-wifi"
+WIFI_KEY = "key1234"
+WEBSOCKET_CONNECTION_URL = "ws://<WS server URI>:<WS server port>/<WS path>"
+SERVER_API_KEY = "<game server's API key>"
+DEVICE_ID = 255  # number, 1-255
